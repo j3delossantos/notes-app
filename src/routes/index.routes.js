e02ebb9863fbe6af448a01f7b2febae39d82cf52
Juan III De los santos <j3delossantos@outlook.com>
2021-01-06 21:58:30 -0400

    Basic Routing configuration and some styles

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
+const indexController = require('../controllers/inidex.controller');
 const router = Router();
+
+router.get('/', indexController.renderIndex);
+
+router.get('/about', indexController.renderAbout);
+
 
 
 module.exports = router;
