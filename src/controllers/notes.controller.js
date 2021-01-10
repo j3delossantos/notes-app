810a51ab3fb19d80710d87e57c65e1997f550e5e
Juan III De los santos <j3delossantos@outlook.com>
2021-01-10 19:21:44 -0400

    add user & active to notes model, - filter by user

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,17 +9,23 @@ notesController.createNewNote = async (req, res) => {
   const newNote = new Note({
     title: req.body.title,
     description: req.body.description,
+    user:req.user.id,
+    active: true
   });
   await newNote.save();
   req.flash("success_msg", "Note Added Succesfully");
   res.redirect("/notes");
 };
 notesController.renderAllNotes = async (req, res) => {
-  const notesResult = await Note.find().lean();
+  const notesResult = await Note.find({user:req.user.id}).sort({createdAt:'desc'}).lean();
   res.render("notes/all-notes", { notesResult });
 };
 notesController.renderEditForm = async (req, res) => {
   const notesResult = await Note.findById(req.params.id).lean();
+  if(notesResult.user != req.user.id){
+    req.flash('error','Not Authorized');
+    return res.redirect('/notes');
+  }
   res.render("notes/edit-note", { notesResult });
 };
 notesController.updateNote = async (req, res) => {
